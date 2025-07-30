f0857014699ada7e257ca947bbadbf843b72aaa2
Terry Zhou <Terry.Zhou@synaptics.com>
2025-07-30 10:07:53 +0800

    configs: syna: use preboot command from defconfig
    Change-Id: I495c53dbfc7e3c60b64d4722567de8b2ab39de95
    Signed-off-by: Terry Zhou <Terry.Zhou@synaptics.com>
    Reviewed-on: https://gerrit-sha.synaptics.com/gerrit/270209

--- include/configs/dolphin.h
+++ include/configs/dolphin.h
@@ -62,6 +62,5 @@
 #define GICC_BASE				0xf7902000
 
 #define CFG_EXTRA_ENV_SETTINGS \
-        "preboot=show_logo; printenv bootcmd\0" \
         "upgrade_available=0\0" \
         "altbootcmd=if test ${boot_slot} = 1; then bootslot set b; bootcount reset;bootcount reset; run bootcmd; else bootslot set a; bootcount reset; bootcount reset; run bootcmd; fi\0"
